db3d60891494854f3ead04ce4638a10f4d630c1e
khan-1995 <khaleel1995@yahoo.com>
2019-03-25 15:29:58 +0530

    initial commit

---
+++ collabarationplatform/src/main/java/com/niit/colab/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.niit.colab.dao;
+
+import java.util.List;
+
+import com.niit.colab.model.UserColab;
+
+
+public interface UserDao {
+	
+	public boolean addCreate(UserColab user);
+	//public UserValidation getCuserid(int cuserid);
+	//public List<UserValidation> retrieveAllUsers();
+	//public UserValidation getUserDetails(String email_id);
+	//public boolean delete(UserValidation user);
+	//public boolean update(UserValidation user);
+
+}
